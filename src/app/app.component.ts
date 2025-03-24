78a0d497910212115b535921e4d6a733ac80e8e7
Pesso <prashant9193034797@gmail.com>
2025-03-24 23:37:46 +0530

    UC2

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+  message="hello from BridgeLabz using one way Data Bindings";
+  logoUrl="assets/logo-bridge.avif";
+}
